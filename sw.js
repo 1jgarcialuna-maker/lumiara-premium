d57ab4836733e644e2ba1cb5557e51325e84604a
JACQUELINE GARCIA <1jgarcialuna@gmail.com>
2026-06-24 10:08:25 -0300

    Add service worker for caching assetsAdicionar sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = "lumiara-v1";
+const ASSETS = [
+  "./",
+  "index.html",
+  "styles.css",
+  "app.js",
+  "manifest.webmanifest",
+  "assets/icon.svg",
+  "assets/hero-lumiara.png",
+  "assets/curadoria-produtos.png",
+  "assets/colecao-ebooks.png",
+  "assets/inspiracao-lumiara.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("index.html")))
+  );
+});
